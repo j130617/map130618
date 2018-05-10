fdf0aaeceeefa5eae7d2532e9ddcfc8f6e18fb47
Ray Assch <reinier_a@hotmail.com>
2018-05-10 16:06:10 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -127,8 +127,8 @@ $(window).on('load', function() {
       if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
-          (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
-          point['Description'] + '<br>' + point['Location']);
+          (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') '<b>' +
+          point['Description'] + '</b><br>' + point['Found']);
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
