87b8552507e90b8cf11347501c7adbac5b4e08ef
j130617 <39986473+j130617@users.noreply.github.com>
2018-06-13 14:02:49 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,2 +1,3 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1sCYzTygSU1z6B_i5suOK4phrStchamdI2Q2EPg9kvzM/edit#gid=0';
+//var googleDocURL = 'https://docs.google.com/spreadsheets/d/1sCYzTygSU1z6B_i5suOK4phrStchamdI2Q2EPg9kvzM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1bGKi0cpF63ks5pnOlnUl_3LIrDpAqtC3m27fQ9SFWBI/edit#gid=0';
 var googleURL = 'https://www.google.com';
