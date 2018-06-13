7493dd0219066fcaac6aafb7e8a0cb577039cfe0
Ray Assch <reinier_a@hotmail.com>
2018-06-13 11:51:35 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -977,7 +977,8 @@ $(window).on('load', function() {
          // Options.csv does not exist, so use Tabletop to fetch data from
          // the Google sheet
          mapData = Tabletop.init({
-           key: googleDocURL,
+           key: data.xlxs
+           //key: googleDocURL,
            callback: function(data, mapData) { onMapDataLoad(); }
          });
        },
